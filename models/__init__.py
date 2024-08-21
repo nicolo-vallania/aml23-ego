f011999cc27058b4c7f8d3942a0f34e884f46d12
nicolo-vallania <130768250+nicolo-vallania@users.noreply.github.com>
2024-08-21 10:01:59 +0200

    chore: update models import

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import Classifier
+from models.GRU_RGB import GRU_RGB
+from models.LSTM_RGB import LSTM_RGB
+from models.MLP import MLP
